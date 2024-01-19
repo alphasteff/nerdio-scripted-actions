0e09b90283811e494999afaf2d89ef933e8ea3b1
Stefan Beckmann <17079526+alphasteff@users.noreply.github.com>
2024-01-19 15:27:29 +0100

    Add new Scripted Action

---
+++ windows-scripts/Install Misson Control Proxy Certificate.ps1
@@ -0,0 +1,30 @@
+﻿#name: Install Misson Control Proxy Certificate
+#description: Installs the Mission Control Proxy certificate to the Trusted Root Certification Authorities store
+#execution mode: Individual with restart
+#tags: baseVISION, GoldMaster
+
+<# Notes:
+
+Use this script to install the Mission Control Proxy certificate to the Trusted Root Certification Authorities store.
+
+#>
+
+$ErrorActionPreference = 'Stop'
+
+
+$url = "https://example.com/certificate.crt"
+$certFilePath = "$Env:SystemRoot\Temp\certificate.crt"
+
+# Download the certificate file
+Invoke-WebRequest -Uri $url -OutFile $certFilePath
+
+# Import the certificate to the Trusted Root Certification Authorities store
+$params = @{
+    FilePath          = $certFilePath
+    CertStoreLocation = 'Cert:\LocalMachine\Root'
+}
+
+Import-Certificate @params
+
+# Clean up the certificate file
+Remove-Item $certFilePath
